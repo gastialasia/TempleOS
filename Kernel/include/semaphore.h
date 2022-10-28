82274b6d2e785e3a01ba26a181729c2ae3fc9351
pescudeiro <pescudeiro@itba.edu.ar>
2022-10-28 13:49:07 -0300

    Fixed error on sems.h

--- Kernel/include/semaphore.h
+++ Kernel/include/semaphore.h
@@ -12,10 +12,10 @@ extern uint8_t _xchg(uint8_t * lock,int value);
 
 void deleteProcessFromSem(int64_t pid);
 
-int sem_wait(Semaphore * sem);
+int sem_wait(semPointer sem);
 
-int sem_post(Semaphore * sem);
+int sem_post(semPointer sem);
 
-int sem_close(Semaphore * semToClose);
+int sem_close(semPointer semToClose);
 
 #endif
